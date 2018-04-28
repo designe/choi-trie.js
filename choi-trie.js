bd78ac1d694418ae4a518d8294f8979664afe637
designe <hello.designe@facebook.com>
2018-04-28 15:54:53 +0900

    added choi-trie.js (base template)

---
+++ choi-trie.js
@@ -0,0 +1,27 @@
+/**
+ *
+ * choi-trie; trie for choi (based on radix-trie)
+ *
+ * Author : jbear; JI-WOONG CHOI
+ * Contact : http://jbear.co
+ * Copyright @ jbear
+ *
+**/
+
+var ChoiTrie = (function() {
+    function ChoiTrie() {
+        var about = {
+            VERSION : '0.1',
+            AUTHOR : "jbear"
+        };
+        this.root = {};
+    }
+    ChoiTrie.prototype = {
+        add: function(_word, _what) {
+        },
+        search: function(_word) {
+        }
+    };
+
+    return ChoiTrie;
+}());
